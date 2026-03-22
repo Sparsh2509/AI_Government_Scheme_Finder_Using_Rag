44d33a3dc294bcd5027caea947d9ba5acbb49f26
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-22 18:47:59 +0530

    Testing reteiever code

---
+++ rag/retriever.py
@@ -0,0 +1,19 @@
+from langchain_qdrant import Qdrant
+from langchain_huggingface import HuggingFaceEmbeddings
+from qdrant_client import QdrantClient
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+def retrieve_schemes(query):
+    client = QdrantClient(path="./qdrant_db")
+    
+    vectorstore = Qdrant(
+        client=client,
+        collection_name="schemes",
+        embeddings=embeddings
+    )
+    
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+    docs = retriever.invoke(query)
+    
+    return docs
