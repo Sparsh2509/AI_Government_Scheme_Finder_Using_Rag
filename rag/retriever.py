1dbc38626eea2c93d78c3d0ccbcd20f235cdbd95
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-29 18:02:14 +0530

    Changes done in retriever.py acc to cloud db

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,11 +1,17 @@
+import os
 from langchain_qdrant import QdrantVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from qdrant_client import QdrantClient
+from dotenv import load_dotenv
+load_dotenv()
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 def retrieve_schemes(query):
-    client = QdrantClient(path="./qdrant_db")
+    client = QdrantClient(
+        url=os.getenv("QDRANT_URL"),
+        api_key=os.getenv("QDRANT_API_KEY")
+    )
     
     vectorstore = QdrantVectorStore(
         client=client,
